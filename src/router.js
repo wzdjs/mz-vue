4f9445b1e648d424454744428b6ab525d4f1f414
wzdjs <958078744@qq.com>
2019-02-23 11:38:11 +0800

    路由守卫

--- src/router.js
+++ src/router.js
@@ -1,3 +1,5 @@
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 // router.js 就是 vue-router 的配置文件
 
 // 1. 引入 vue
@@ -19,6 +21,10 @@ import Center from './views/Center.vue';
 import City from './views/City.vue';
 import Home from './views/Home.vue';
 import Detail from './views/Detail.vue';
+import Login from './views/Login.vue';
+
+NProgress.configure({ showSpinner: false });
+
 Vue.use(VueRouter);
 
 let router = new VueRouter({
@@ -73,7 +79,52 @@ let router = new VueRouter({
       // redirect地址从定向
       path: '*',
       redirect: '/films'
+    },
+    {
+      path: '/card',
+      component: {
+        render (h) {
+          return h('div', '卖座卡页面')
+        }
+      }
+    },
+    {
+      path: '/money',
+      component: {
+        render (h) {
+          return h('div', '余额页面')
+        }
+      }
+    },
+    {
+      path: '/system',
+      component: {
+        render (h) {
+          return h('div', '设置页面')
+        }
+      }
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ]
+})
+
+// 全局前置守卫
+// next 是否允许去 next()不用或者传入false就不去跳转（拦截），如果想调到某个页面只要传入对应页面的路由就可以了，不传参数就是允许跳转，（不拦截）
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  if (to.path === '/card' || to.path === '/money' || to.path === '/system') {
+    // next(false);
+    next('/login');
+  } else {
+    next();
+  }
+})
+
+// 全局后置守卫
+router.afterEach((to, from) => {
+  NProgress.done();
 })
 export default router;
--- src/views/Center.vue
+++ src/views/Center.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>我是个人中心页面</h1>
+    <ul>
+      <router-link tag="li" to="/card">卖座卡</router-link>
+      <router-link tag="li" to="money">余额</router-link>
+      <router-link tag="li" to="system">设置</router-link>
+    </ul>
   </div>
 </template>
 
